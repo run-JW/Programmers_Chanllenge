662eed7a3c2d34b100580ebdc4df773ff9a6955d
run-JW <65688438+run-JW@users.noreply.github.com>
2021-06-23 23:22:44 +0900

    Create 예산.js

---
+++ Level1/예산.js
@@ -0,0 +1,13 @@
+const solution = (d, budget) => {
+    const goods = [];
+    const min = Math.min(d);
+
+    while(budget < min) {
+        
+       
+    }
+
+    return answer;
+}
+
+const d = [1,3,2,5,4], budget = 9;
