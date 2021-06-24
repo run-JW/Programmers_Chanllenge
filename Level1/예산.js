00618894a03a570ed606f0376a9af0f58765f2b8
run-JW <65688438+run-JW@users.noreply.github.com>
2021-06-24 11:00:39 +0900

    Update 예산.js

--- Level1/예산.js
+++ Level1/예산.js
@@ -1,13 +1,15 @@
+// Best case가 가장 적은 수를 먼저 골랐을 때 나온다고 생각.
+// 오름차순 정렬한 뒤, 앞의 것부터 예산을 차감한 카운트 수를 반환
 const solution = (d, budget) => {
-    const goods = [];
-    const min = Math.min(d);
-
-    while(budget < min) {
-        
-       
+    let count = 0;
+    d.sort((a, b) => a - b);
+    while(true) {
+        budget -= d.shift();
+        if(budget > 0) count++;
+        else if(budget === 0) {
+            count++
+            return count;
+        }
+        else return count;
     }
-
-    return answer;
 }
-
-const d = [1,3,2,5,4], budget = 9;
