fe11585d11f34772e8bbb8dac3a101d9b348f234
run-JW <65688438+run-JW@users.noreply.github.com>
2021-07-21 10:23:54 +0900

    Update 올바른 괄호.js

--- Level2/올바른 괄호.js
+++ Level2/올바른 괄호.js
@@ -1,3 +1,4 @@
+/*
 function solution(s){
     const stack = [];
 
@@ -10,4 +11,15 @@ function solution(s){
         }
     }
     return stack.length === 0 ? true : false;
+}
+*/
+
+function solution(s) {
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') stack.push(s[i]);
+        else if (stack.length) stack.pop();
+        else return false; 
+    }
+    return stack.length ? false : true;
 }
