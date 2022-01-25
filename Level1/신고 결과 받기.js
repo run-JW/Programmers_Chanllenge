016bedfe77044389d0b3c6363abf11ea5e736713
run-JW <65688438+run-JW@users.noreply.github.com>
2022-01-25 23:54:54 +0900

    Update 신고 결과 받기.js

--- Level1/신고 결과 받기.js
+++ Level1/신고 결과 받기.js
@@ -1,8 +1,18 @@
 function solution(id_list, report, k) {
+    const realReport = Array.from(new Set(report))
+                            .join(' ')
+                            .split(' ');
+    const dataCenter = {};
     
-    const realReport = Array.from(new Set(report));
+    while (realReport.length) {
+        const crimer = realReport.pop();
+        const reporter = realReport.pop();
+        
+        if (!dataCenter[`${crimer}`])
+            dataCenter[`${crimer}`] = [];
+        
+        dataCenter[`${crimer}`].push(reporter);
+    }
     
-    console.log(realReport.length);
     
-    return report;
 }
