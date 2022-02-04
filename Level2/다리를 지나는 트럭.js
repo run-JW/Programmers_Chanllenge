5b4c1a822581294e0ad80acd562a0b52a4e48164
run-JW <65688438+run-JW@users.noreply.github.com>
2022-02-04 22:28:31 +0900

    Update 다리를 지나는 트럭.js

--- Level2/다리를 지나는 트럭.js
+++ Level2/다리를 지나는 트럭.js
@@ -1,4 +1,25 @@
 function solution(bridge_length, weight, truck_weights) {
-    let  answer = 0;
-    return answer;
+    const onBridge = [];
+    let seconds = 0;
+    
+    while (truck_weights.length || onBridge.length) {
+        seconds++;
+        if (onBridge.length) {
+            const curWeight = onBridge.reduce(sum);
+            if (curWeight + truck_weights[0] <= weight) {
+                onBridge.push(truck_weights.shift());
+            }
+        } else {
+            onBridge.push(truck_weights.shift());
+        }
+        if (onBridge.reduce(sum) + truck_weights[0] <= weight) {
+            onBridge.push(truck_weights.shift());
+        }
+        if (seconds % bridge_length === 0)
+            onBridge.shift();
+    }
+    
+    return seconds;
 }
+    
+const sum = (x, y) => x + y;
